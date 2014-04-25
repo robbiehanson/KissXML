dc11433262d4d483b19966ada79300eb9603f24c
Will Bartlett <will118@gmail.com>
2014-04-25 13:51:22 +0100

    Added ifdef for CTidy

--- KissXML/DDXMLDocument.m
+++ KissXML/DDXMLDocument.m
@@ -1,5 +1,8 @@
 #import "DDXMLPrivate.h"
 #import "NSString+DDXML.h"
+#ifdef PAPERS_APP_IOS
+    #import "CTidy.h"
+#endif
 
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
