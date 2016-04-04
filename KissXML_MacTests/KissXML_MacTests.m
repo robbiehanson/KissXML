5d4216553c2ecb291c6020e0ab901e06ce13eddc
TuanNguyen <anhtuanpk2000@gmail.com>
2016-04-04 17:22:00 +0700

    Add Mac support, remove swift header

---
+++ KissXML_MacTests/KissXML_MacTests.m
@@ -0,0 +1,39 @@
+//
+//  KissXML_MacTests.m
+//  KissXML_MacTests
+//
+//  Created by Nguyen Tuan on 3/28/16.
+//  Copyright © 2016 co.pku. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KissXML_MacTests : XCTestCase
+
+@end
+
+@implementation KissXML_MacTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
