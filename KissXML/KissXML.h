e83e87540e74183bc9aeb108f605bc03241632cb
Marek Cirkos <marek@cirkos.me>
2016-04-12 23:12:20 +0100

    Added Mac & iOS framework products with shared scheme used by Carthage to build them.

---
+++ KissXML/KissXML.h
@@ -0,0 +1,35 @@
+/**
+ * Welcome to KissXML.
+ *
+ * The project page has documentation if you have questions.
+ * https://github.com/robbiehanson/KissXML
+ *
+ * If you're new to the project you may wish to read the "Getting Started" wiki.
+ * https://github.com/robbiehanson/KissXML/wiki/GettingStarted
+ *
+ * KissXML provides a drop-in replacement for Apple's NSXML class cluster.
+ * The goal is to get the exact same behavior as the NSXML classes.
+ *
+ * For API Reference, see Apple's excellent documentation,
+ * either via Xcode's Mac OS X documentation, or via the web:
+ *
+ * https://github.com/robbiehanson/KissXML/wiki/Reference
+ **/
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KissXML.
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+
+//! Project version string for KissXML.
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
+
+
+#import <KissXML/DDXML.h>
+#import <KissXML/DDXMLDocument.h>
+#import <KissXML/DDXMLElement.h>
+#import <KissXML/DDXMLElementAdditions.h>
+#import <KissXML/DDXMLNode.h>
+#import <KissXML/NSString+DDXML.h>
